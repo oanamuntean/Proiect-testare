2b8b47a8663572cb3b673e66d73bd3bb9718d986
OANA-DIANA MUNTEAN <muntean.oana165313@student.ubbcluj.ro>
2020-11-28 17:35:10 +0200

    Add project files.

---
+++ Data/PrtestareContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Prtestare.Models;
+
+namespace Prtestare.Data
+{
+    public class PrtestareContext : DbContext
+    {
+        public PrtestareContext (DbContextOptions<PrtestareContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Prtestare.Models.Movie> Movie { get; set; }
+    }
+}
